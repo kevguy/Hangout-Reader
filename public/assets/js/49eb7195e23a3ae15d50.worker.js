dbe2dd65005d0292881ec3405ea29c232f80131c
kevguy <kevlai22@uw.edu>
2017-04-03 14:06:41 +0800

    chore: update path

--- public/assets/js/49eb7195e23a3ae15d50.worker.js
+++ public/assets/js/49eb7195e23a3ae15d50.worker.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "./public/assets/js/";
+/******/ 	__webpack_require__.p = "Hangout-Reader/public/assets/js/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
