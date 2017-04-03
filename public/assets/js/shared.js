f2627bd0a07ce937c161a9ac1a251bcbb49aecb4
kevguy <kevlai22@uw.edu>
2017-04-03 16:47:17 +0800

    fix: file path

--- public/assets/js/shared.js
+++ public/assets/js/shared.js
@@ -30,7 +30,7 @@
 /******/ 	// "0" means "already loaded"
 /******/ 	// Array means "loading", array contains callbacks
 /******/ 	var installedChunks = {
-/******/ 		1:0
+/******/ 		2:0
 /******/ 	};
 
 /******/ 	// The require function
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"about"}[chunkId]||chunkId) + "_page.js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"about","1":"main"}[chunkId]||chunkId) + "_page.js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
